9f994e6297babbcd7ad8273d2ec5b672fb06539e
Hiran Oshadha <128114334+hiranoshadha@users.noreply.github.com>
2024-12-03 15:16:24 +0530

    Final Project

---
+++ lib/routes/routes.dart
@@ -0,0 +1,79 @@
+import 'package:go_router/go_router.dart';
+import 'package:zero_waste/screens/binManagement/assign_route.dart';
+import 'package:zero_waste/screens/binManagement/bin_view_screen.dart';
+import 'package:zero_waste/screens/binManagement/create_bin_screen.dart';
+import 'package:zero_waste/screens/binManagement/create_route_screen.dart';
+import 'package:zero_waste/screens/garbageCollection/record_garage_entry_user_screen.dart';
+import 'package:zero_waste/screens/garbageCollection/user_history_screen.dart';
+import 'package:zero_waste/screens/home_screen.dart';
+import 'package:zero_waste/screens/onBoarding_screen.dart';
+import 'package:zero_waste/screens/rewardingSystem/item_dashboard.dart';
+import 'package:zero_waste/screens/rewardingSystem/manage_points.dart';
+import 'package:zero_waste/screens/rewardingSystem/manage_rewards_screen.dart';
+import 'package:zero_waste/screens/rewardingSystem/rewards_gift.dart';
+import 'package:zero_waste/screens/userManagement/add_employee_screen.dart';
+import 'package:zero_waste/screens/userManagement/login_screen.dart';
+import 'package:zero_waste/screens/splash_screen.dart';
+import 'package:zero_waste/screens/userManagement/register_screen.dart';
+import 'package:zero_waste/screens/userManagement/user_dashboard_screen.dart';
+import 'package:zero_waste/screens/userManagement/user_update_screen.dart';
+
+import '../models/user.dart';
+
+class AppRouter {
+  static final router = GoRouter(
+    routes: [
+      //General Routes
+      GoRoute(path: '/', builder: (context, state) => SplashScreen()),
+      GoRoute(path: '/onboard', builder: (context, state) => const OnboardingScreen()),
+      GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
+
+      //User Management Routes
+      GoRoute(path: '/user/login', builder: (context, state) => const LoginScreen()),
+      GoRoute(
+          path: '/user/register',
+          builder: (context, state) => const RegisterScreen()),
+      GoRoute(
+          path: '/user/update',
+          builder: (context, state) => const UserUpdateScreen()),
+      GoRoute(
+          path: '/employee/register',
+          builder: (context, state) => const AddEmployeeScreen()),
+
+      //Bin Management Routes
+      GoRoute(path: '/bin/view', builder: (context, state) => BinViewScreen(loginedUser: User(email: '', password: ''))),
+      GoRoute(
+          path: '/bin/create', builder: (context, state) => const CreateBinScreen()),
+      GoRoute(
+          path: '/route/create', builder: (context, state) => const CreateRouteScreen()),
+      GoRoute(
+          path: '/route/assign', builder: (context, state) => const AssignRoute()),
+
+      //Garbage Collection Routes
+      GoRoute(
+          path: '/collection/entry',
+          builder: (context, state) => const RecordGarbageEntryScreen()),
+      GoRoute(
+          path: '/collection/history',
+          builder: (context, state) => const UserHistoryScreen()),
+
+      //Reward Management Routes
+      GoRoute(
+          path: '/reward/manage',
+          builder: (context, state) => const ManageRewardsScreen()),
+      GoRoute(
+          path: '/reward/points',
+          builder: (context, state) => const PointsCreateScreen()),
+      GoRoute(
+          path: '/reward/gifts',
+          builder: (context, state) => const RewardsGiftScreen()),
+      GoRoute(
+          path: '/reward/items',
+          builder: (context, state) => const AdminDashboard()),
+
+      GoRoute(
+          path: '/profile',
+          builder: (context, state) => const UserDashboardScreen()),
+    ],
+  );
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:zero_waste/providers/user_provider.dart';
+import 'package:zero_waste/screens/binManagement/schedule_details.dart';
+import 'package:zero_waste/widgets/dialog_messages.dart';
+import 'package:zero_waste/widgets/submit_button.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+    final user = userProvider.user;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Temporary Home Screen'),
+        foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+        backgroundColor: Colors.green.shade700,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFa8e063), Color(0xFF56ab2f)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              elevation: 8,
+              shadowColor: Colors.green.shade200,
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 20),
+                    user != null
+                        ? Text('Logged in as: ${user.email}')
+                        : Text('No user logged in'),
+                    const SizedBox(height: 20),
+                    _buildDashboardButton(
+                      context: context,
+                      text: 'Login',
+                      icon: Icons.login,
+                      route: '/user/login',
+                    ),
+                    const SizedBox(height: 20),
+                    _buildDashboardButton(
+                      context: context,
+                      text: 'User profile',
+                      icon: Icons.history,
+                      route: '/profile',
+                    ),
+                    const SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    onPressed: () => executeMethod(context),
+                    icon: Icon(Icons.add_circle_outline),
+                    label: const Text("Print"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                    const SizedBox(height: 20),
+                    _buildDashboardButton(
+                      context: context,
+                      text: 'Record Garbage Entry',
+                      icon: Icons.add_circle_outline,
+                      route: '/collection/entry',
+                    ),
+                    const SizedBox(height: 20),
+                    _buildDashboardButton(
+                      context: context,
+                      text: 'User History',
+                      icon: Icons.history,
+                      route: '/collection/history',
+                    ),
+                    const SizedBox(height: 20),
+                    _buildDashboardButton(
+                      context: context,
+                      text: 'Points',
+                      icon: Icons.history,
+                      route: '/reward/points',
+                    ),
+                    const SizedBox(height: 20),
+                    _buildDashboardButton(
+                      context: context,
+                      text: 'Gifts',
+                      icon: Icons.history,
+                      route: '/reward/gifts',
+                    ),
+                    const SizedBox(height: 20),
+                    _buildDashboardButton(
+                      context: context,
+                      text: 'Items',
+                      icon: Icons.history,
+                      route: '/reward/items',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDashboardButton({
+    required BuildContext context,
+    required String text,
+    required IconData icon,
+    required String route,
+  }) {
+    return ElevatedButton.icon(
+      onPressed: () => context.go(route),
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        elevation: 5,
+        backgroundColor: Colors.green.shade600,
+        shadowColor: Colors.green.shade300,
+      ),
+      icon: Icon(
+        icon,
+        color: Colors.white,
+        size: 24,
+      ),
+      label: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  executeMethod(BuildContext context) {
+    ScheduleDetails().getDetails(context).then((_){
+      print("YO YO");
+    }).catchError((error){
+      okMessageDialog(context, "Failed", error.toString());
+    });
+  }
+}
